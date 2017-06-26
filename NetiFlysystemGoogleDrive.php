ae9ee91df3a3a3c7521df80caeda4304dd45dd2e
Benjamin Müller <bm@netinventors.de>
2017-06-26 15:09:25 +0200

    add composer auto loader

--- NetiFlysystemGoogleDrive.php
+++ NetiFlysystemGoogleDrive.php
@@ -9,6 +9,10 @@ namespace NetiFlysystemGoogleDrive;
 require __DIR__ . '/vendor/autoload.php';
 
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UpdateContext;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * Class NetiFlysystemGoogleDrive
@@ -17,4 +21,70 @@ use Shopware\Components\Plugin;
  */
 class NetiFlysystemGoogleDrive extends Plugin
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $pathToComposerAutoloader = $this->getComposerAutoloader();
+        $container->setParameter(
+            $this->getContainerPrefix() . '.composer_autoloader_path',
+            $pathToComposerAutoloader
+        );
+        parent::build($container);
+
+        if (is_file($pathToComposerAutoloader)) {
+            require $pathToComposerAutoloader;
+        }
+    }
+
+    /**
+     * @param InstallContext $context
+     *
+     * @throws \Exception
+     */
+    public function install(InstallContext $context)
+    {
+        $this->hasComposerAutoloader();
+        parent::install($context);
+    }
+
+    /**
+     * @param UpdateContext $context
+     *
+     * @throws \Exception
+     */
+    public function update(UpdateContext $context)
+    {
+        $this->hasComposerAutoloader();
+        parent::update($context);
+    }
+
+    /**
+     * @param ActivateContext $context
+     *
+     * @throws \Exception
+     */
+    public function activate(ActivateContext $context)
+    {
+        $this->hasComposerAutoloader();
+        parent::activate($context);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function hasComposerAutoloader()
+    {
+        $pathToComposerAutoloader = $this->getComposerAutoloader();
+
+        if (! is_file($pathToComposerAutoloader)) {
+            throw new \Exception('Please run "composer install" before you install the plugin!');
+        }
+    }
+
+    private function getComposerAutoloader()
+    {
+        return $this->getPath() . '/vendor/autoload.php';;
+    }
 }
