c287202be26211a299376a9783e089382d9f50dc
Benjamin Müller <bm@netinventors.de>
2017-06-26 14:40:44 +0200

    init plugin

---
+++ NetiFlysystemGoogleDrive.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright  Copyright (c) 2017, Net Inventors GmbH
+ * @category   NetiFlysystemGoogleDrive
+ * @author     bmueller
+ */
+
+namespace NetiFlysystemGoogleDrive;
+require __DIR__ . '/vendor/autoload.php';
+
+use Shopware\Components\Plugin;
+
+/**
+ * Class NetiFlysystemGoogleDrive
+ *
+ * @package NetiFlysystemGoogleDrive
+ */
+class NetiFlysystemGoogleDrive extends Plugin
+{
+}
